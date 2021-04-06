a8b4846734f8da8a0646b0d861dfda4d2e65c251
howordFEAKEK <jong9181@gmail.com>
2021-04-06 18:02:28 +0900

    add api add gps
    21 04 06

---
+++ GoTrips/app/src/main/java/com/example/project_trip/Transfer.java
@@ -0,0 +1,15 @@
+package com.example.project_trip;
+
+public class Transfer {
+    String trans(String str) {
+
+        str = str.replaceAll("&gt;", "> ")
+                .replaceAll("&#xD;", " ")
+                .replaceAll("&lt;", " <")
+                .replaceAll("&lt;;", " <");
+
+
+        return str;
+
+    }
+}
