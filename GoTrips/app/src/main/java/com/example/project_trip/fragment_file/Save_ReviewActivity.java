5bc388f906ec672e68c83e345c940efc984905f5
pkh2351 <76101223+fkdla1963@users.noreply.github.com>
2021-05-27 04:55:55 +0900

    주석 설명 추가

--- GoTrips/app/src/main/java/com/example/project_trip/fragment_file/Save_ReviewActivity.java
+++ GoTrips/app/src/main/java/com/example/project_trip/fragment_file/Save_ReviewActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.project_trip.R;
@@ -16,18 +17,20 @@ import com.example.project_trip.R;
     // 리뷰작성 버튼을 클릭하면 나오는 엑티비티
 
 
-    TextView txt , txt_guide , txt_sido , txt_gungu;
-
+    TextView txt_save , txt_guide , txt_sido , txt_gungu;
+    EditText etxt_set_review , etxt_set_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_review);
 
-        txt = findViewById(R.id.save_click_text1);
-        txt_guide = findViewById(R.id.set_Review_guide);
-        txt_sido = findViewById(R.id.set_Review_sido);
-        txt_gungu = findViewById(R.id.set_Review_gungu);
+        txt_save = findViewById(R.id.save_click_text1);          // 맨 밑의 저장하기 글
+        txt_guide = findViewById(R.id.set_Review_guide);    // 관광지 이름 텍스트
+        txt_sido = findViewById(R.id.set_Review_sido);      // 시,도
+        txt_gungu = findViewById(R.id.set_Review_gungu);    // 군,구
         
+        etxt_set_title = findViewById(R.id.save_review_edittitle);//리뷰 제목 에딧텍스트
+        etxt_set_review = findViewById(R.id.set_review_review); // 리뷰 내용 에딧텍스트
         // Local_guide에서 값 가져오기
         Intent intent = getIntent();
         txt_guide.setText(intent.getStringExtra("관광지명"));
@@ -36,7 +39,7 @@ import com.example.project_trip.R;
         
         
         // 저장하기 버튼 클릭 이벤트
-        txt.setOnClickListener(new View.OnClickListener() {
+        txt_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder ad = new AlertDialog.Builder(Save_ReviewActivity.this);
@@ -44,18 +47,21 @@ import com.example.project_trip.R;
                 ad.setTitle("제목");
                 ad.setMessage("추가 하겠습니까?");
 
-                ad.setPositiveButton("취소", new DialogInterface.OnClickListener() {
+                ad.setPositiveButton("추가", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
+                    // 서버 통신 코드 작성...
+
                 });
 
-                ad.setNegativeButton("추가", new DialogInterface.OnClickListener() {
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
+                
                 });
                 ad.show();
 
