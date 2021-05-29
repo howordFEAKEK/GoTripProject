a40b706aebdb04803381593981e3c60adab33eee
pkh2351 <76101223+fkdla1963@users.noreply.github.com>
2021-05-29 16:22:32 +0900

    개인맞춤탭 관광지 추천 리스트 작업

--- GoTrips/app/src/main/java/com/example/project_trip/CusmaidFragment.java
+++ GoTrips/app/src/main/java/com/example/project_trip/CusmaidFragment.java
@@ -16,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,6 +32,7 @@ import com.example.project_trip.fragment_file.RecyclerViewAdapter_from_local_gui
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,12 +43,19 @@ public class CusmaidFragment extends Fragment {
     // 개인맞춤 탭에 있는 프레그 먼트 입니다.
     myDBHelper myHelper;
     SQLiteDatabase sqlDB;
-    TextView tv1 , txttest1 , txttest2;
+    TextView tv1 , txttest1 , txttest2, cusmaid_local_title;
     EditText edit1 , edit2;
     RecyclerView marylee , marylee2;
     RecyclerViewAdapter_from_local_guide rcvAd;
-    List<Main_item_from_show_local> getMyList;
+    private List<Main_item_from_show_local> getMyList;
+    private List<Cusmaid_title_item> getItemLocal;
     Button btnSelect , btnInit;
+
+    String val ,val2 ,val3 , test1 , test2;
+
+    Getter getter = new Getter();
+    Cutter cutter = new Cutter();
+
     public CusmaidFragment() {
         // Required empty public constructor
     }
@@ -56,18 +65,23 @@ public class CusmaidFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        //API사용할때 팅기지 않게 하는거
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }//...끝
         ViewGroup vv = (ViewGroup) inflater.inflate(R.layout.fragment_cusmaid, container, false);
         marylee = (RecyclerView) vv.findViewById(R.id.Cusmaid_recyclerView);
 
+        cusmaid_local_title = vv.findViewById(R.id.cusmaid_local_title);
         btnSelect = vv.findViewById(R.id.btnSelect);
         btnInit = vv.findViewById(R.id.button4);
 
-        rcvAd = new RecyclerViewAdapter_from_local_guide(getContext(), getMyList);
-        marylee.setLayoutManager(new LinearLayoutManager(getActivity()));
-        marylee.setAdapter(rcvAd);
+//        rcvAd = new RecyclerViewAdapter_from_local_guide(getContext(), getMyList);
+//        marylee.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        marylee.setAdapter(rcvAd);
 
-//        myHelper = new myDBHelper(getContext());
+        myHelper = new myDBHelper(getContext());
         //조회 누를시
         btnSelect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -98,7 +112,7 @@ public class CusmaidFragment extends Fragment {
 //                sqlDB.close();
 //            }
         });
-
+        myHelper = new myDBHelper(getContext());
         // 2021-04-10 추가, 내부 DB이용 자주 검색한 지역 찾기
         btnInit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -119,12 +133,56 @@ public class CusmaidFragment extends Fragment {
                 String[] buf = strNames.split("\n");
                 String[] buf2 = strNumbers.split("\n");
                 Toast.makeText(getContext(),"자주 조회하신 도시는 "+buf[0]+"입니다.\n자주 조회하신 군구는 "+buf2[0]+"입니다.", Toast.LENGTH_LONG).show();
-                String val = buf[0];
+                val = buf[1];
+                val2 = buf2[0];
+                val3 = val + " " +val2;
+
+                cusmaid_local_title.setText(val3);
+
                 cursor.close();
                 sqlDB.close();
+                Log.d("시도" , val);
+                Log.d("군구" , val2);
+                Log.d("합" , val3);
+                if(cusmaid_local_title.getText().toString().equals(val + " " + val2)) {
 
+
+                    try {
+
+
+                        String name = val;
+                        String name2 = val2;
+                        Log.d("시도11" , name);
+                        Log.d("군구11" , name2);
+                        Log.d("시도22" , "서울특별시");
+                        Log.d("군구22" , "강남구");
+                        test1 = getter.apiGetter("서울특별시", "강남구");
+                        Log.d("test1" , test1);
+                        test2 = cutter.apiCutter(test1, "BResNm");
+                        Log.d("test2" , test2);
+                        String str = test2;
+                        String[] target = str.split("\n");
+                        getMyList = new ArrayList<>();
+                        for (int i = 0; i < target.length; i++) {
+                            Log.d("target" , target[i]);
+                            getMyList.add(new Main_item_from_show_local(target[i]));
+
+                        }
+
+                        rcvAd = new RecyclerViewAdapter_from_local_guide(getContext(), getMyList);
+                        marylee.setLayoutManager(new LinearLayoutManager(getActivity()));
+                        marylee.setAdapter(rcvAd);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
             }
+
         });
+
+
         return vv;
 
     }
@@ -159,12 +217,12 @@ public class CusmaidFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getMyList = new ArrayList<>();
-        getMyList.add(new Main_item_from_show_local("경기도 화성 박물관"));
-        getMyList.add(new Main_item_from_show_local("제주도 돌하르방 박물관"));
-        getMyList.add(new Main_item_from_show_local("제주도 한라산 박물관"));
-        getMyList.add(new Main_item_from_show_local("제주도 올레길"));
-        getMyList.add(new Main_item_from_show_local("경주 박물관"));
+//        getMyList = new ArrayList<>();
+//        getMyList.add(new Main_item_from_show_local("경기도 화성 박물관"));
+//        getMyList.add(new Main_item_from_show_local("제주도 돌하르방 박물관"));
+//        getMyList.add(new Main_item_from_show_local("제주도 한라산 박물관"));
+//        getMyList.add(new Main_item_from_show_local("제주도 올레길"));
+//        getMyList.add(new Main_item_from_show_local("경주 박물관"));
 
     }
 }
---
+++ GoTrips/app/src/main/java/com/example/project_trip/Cusmaid_title_item.java
@@ -0,0 +1,12 @@
+package com.example.project_trip;
+
+public class Cusmaid_title_item {
+
+    public String sido_name;
+    public String gungu_name;
+
+    public Cusmaid_title_item(String sido_name, String gungu_name) {
+        this.sido_name = sido_name;
+        this.gungu_name = gungu_name;
+    }
+}
