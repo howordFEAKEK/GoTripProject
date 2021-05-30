724885432e73a66e3851ea3ceba0fca6188b2d6a
gitPlayLee <lion7542@naver.com>
2021-05-30 10:50:19 +0900

    리뷰 차트 수정 완료, 주간 차트 부분 문제 해결중

--- GoTrips/app/src/main/java/com/example/project_trip/fragment_file/RecyclerViewAdapter.java
+++ GoTrips/app/src/main/java/com/example/project_trip/fragment_file/RecyclerViewAdapter.java
@@ -44,12 +44,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     // 뷰홀더가 묶여있을 때 ( 데이터와 요소들을 연결 )
-    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
-
-        Main_item temp = miData.get(position);
-
-        holder.tv_list.setText(miData.get(position).getList());
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        //Main_item temp = miData.get(position);
+        holder.tv_list.setText(miData.get(position).review_index_title);
 //        holder.tv_imglist.setImageResource(miData.get(position).getImg());
+/*
         holder.tv_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,21 +57,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 mContext.startActivity(intent);
             }
         });
-
+*/
 
     }
 
     @Override
     // 목록 아이템 수
-    public int getItemCount() {
-
-        return miData.size();
-    }
+    public int getItemCount() {return miData.size();}
 
 
     // 뷰 홀더 클래스
-
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView tv_list;
 //        private ImageView tv_imglist;
@@ -82,12 +77,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
             tv_list = itemView.findViewById(R.id.title_id111);
 //            this.tv_imglist = itemView.findViewById(R.id.icon_rv);
-
-
-
+            itemView.setOnClickListener(this);
         }
 
-
-
+        @Override
+        public void onClick(View v) {
+            int postion = getAdapterPosition();
+            String wirter = miData.get(postion).review_index_writername;
+            String wrDate = miData.get(postion).review_index_date;
+            String title = miData.get(postion).review_index_title;
+            Intent intent = new Intent(mContext , Show_ReviewActivity.class);
+            intent.putExtra("title" , title);
+            intent.putExtra("wirter", wirter);
+            intent.putExtra("wrdate", wrDate);
+            mContext.startActivity(intent);
+        }
     }
 }
