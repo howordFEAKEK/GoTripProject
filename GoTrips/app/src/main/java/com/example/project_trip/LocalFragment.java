e7bebb969b1962b27247084c314619af975ba65f
pkh2351 <76101223+fkdla1963@users.noreply.github.com>
2021-06-01 00:13:05 +0900

    내부 DB에 위치값 관광지 저장 테스트

--- GoTrips/app/src/main/java/com/example/project_trip/LocalFragment.java
+++ GoTrips/app/src/main/java/com/example/project_trip/LocalFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +23,18 @@ import com.example.project_trip.fragment_file.RecyclerViewAdapter_from_local_gui
 import com.example.project_trip.fragment_file.RecyclerViewAdapter_from_show_local;
 import com.squareup.otto.Subscribe;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -96,14 +108,134 @@ public class LocalFragment extends Fragment {
 
         if (tv_local_selected.getText().toString().equals(name + " " + name2)) {
 
-            try {
-                test1 = getter.apiGetter(name, name2);
-                test2 = cutter.apiCutter(test1, "BResNm");
+//            try {
+//                test1 = getter.apiGetter(name, name2);
+//                test2 = cutter.apiCutter(test1, "BResNm");
+//
+//                String str = test2;
+//                String[] target = str.split("\n");
+//                getMyList2 = new ArrayList<>();
+//                Local_Data_List.local_tour_data_list = new ArrayList<>();
+//                for (int i = 0; i < target.length; i++) {
+//                    Main_item_from_show_local item = new Main_item_from_show_local();
+//                    item.sido_name = name; // 시, 도
+//                    item.gungu_name = name2; // 군, 구
+//                    item.tour_location = loca;
+//                    item.tour_title = target[i];
+//                    Local_Data_List.local_tour_data_list.add(item);
+//                    //getMyList2.add(new Main_item_from_show_local(target[i]));
+//                }
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
 
-                String str = test2;
-                String[] target = str.split("\n");
-                getMyList2 = new ArrayList<>();
-                Local_Data_List.local_tour_data_list = new ArrayList<>();
+            // 테스트 시작
+            try {
+            String val = null;
+            String fileName = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+name + " " + name2+".txt";
+            File file = new File(fileName);
+            boolean isExists = file.exists();
+
+            if(isExists) {
+                //파일이 있다면
+                Log.d("","파일읽기");
+                try{
+                    StringBuffer strBuffer = new StringBuffer();
+
+                    InputStream is = new FileInputStream(fileName);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    String line="";
+                    while((line=reader.readLine())!=null){
+                        strBuffer.append(line+"\n");
+                    }
+
+                    reader.close();
+                    is.close();
+                    val=strBuffer.toString();
+                    Log.d("read test",val);
+
+                    String test1111 = cutter.apiCutter(val , "BResNm");
+                    String str = test1111;
+                    String[] target = str.split("\n");
+                    getMyList2 = new ArrayList<>();
+                    Local_Data_List.local_tour_data_list = new ArrayList<>();
+                    for (int i = 0; i < target.length; i++) {
+                        Main_item_from_show_local item = new Main_item_from_show_local();
+                        item.sido_name = name; // 시, 도
+                        item.gungu_name = name2; // 군, 구
+                        item.tour_location = loca;
+                        item.tour_title = target[i];
+                        Local_Data_List.local_tour_data_list.add(item);
+                        //getMyList2.add(new Main_item_from_show_local(target[i]));
+
+                        rcvAd2 = new RecyclerViewAdapter_from_show_local(getContext(), Local_Data_List.local_tour_data_list);
+                        marylee.setLayoutManager(new LinearLayoutManager(getActivity()));
+                        marylee.setAdapter(rcvAd2);
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+
+            }else {
+                Log.d("","파일쓰기");
+                //파일이 없다면
+                // 도시의 이름과 군구를 입력 받고 그에 해당하는 관광지를 전송함
+                StringBuilder urlBuilder = new StringBuilder(
+                        "http://openapi.tour.go.kr/openapi/service/TourismResourceService/getTourResourceList"); /* URL */
+                urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8")
+                        + "=HljdApr9dt8GOT0E0H87Ii2Ah7buUCv6SHh5AmjZJLwWZvRO9Ww4hgPgXehtDS3Ytn3hxKWk9J6sVS4MHdURJA%3D%3D"); /*
+                 * Service
+                 * Key
+                 */
+                urlBuilder
+                        .append("&" + URLEncoder.encode("SIDO", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8")); /* 시도 */
+                urlBuilder.append(
+                        "&" + URLEncoder.encode("GUNGU", "UTF-8") + "=" + URLEncoder.encode(name2, "UTF-8")); /* 시군구 */
+                urlBuilder.append(
+                        "&" + URLEncoder.encode("RES_NM", "UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /* 관광지 */
+                URL url = new URL(urlBuilder.toString());
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-type", "application/json");
+                System.out.println("Response code: " + conn.getResponseCode());
+                BufferedReader rd;
+                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                } else {
+                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                val = sb.toString();
+                rd.close();
+                conn.disconnect();
+
+                //파일 output stream 생성
+                FileOutputStream fos = new FileOutputStream(fileName);
+                //파일쓰기
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+                writer.write(sb.toString());
+                writer.flush();
+                Log.d("write test",sb.toString());
+                String val1 = sb.toString();
+
+                writer.close();
+                fos.close();
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+                        String test1111 = cutter.apiCutter(val1 , "BResNm");
+                        String str = test1111;
+                         String[] target = str.split("\n");
+                         getMyList2 = new ArrayList<>();
+                         Local_Data_List.local_tour_data_list = new ArrayList<>();
                 for (int i = 0; i < target.length; i++) {
                     Main_item_from_show_local item = new Main_item_from_show_local();
                     item.sido_name = name; // 시, 도
@@ -112,15 +244,28 @@ public class LocalFragment extends Fragment {
                     item.tour_title = target[i];
                     Local_Data_List.local_tour_data_list.add(item);
                     //getMyList2.add(new Main_item_from_show_local(target[i]));
+
+                    rcvAd2 = new RecyclerViewAdapter_from_show_local(getContext(), Local_Data_List.local_tour_data_list);
+                    marylee.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    marylee.setAdapter(rcvAd2);
                 }
 
-            } catch (IOException e) {
-                e.printStackTrace();
+//                    }
+//                }).start();
             }
 
-            rcvAd2 = new RecyclerViewAdapter_from_show_local(getContext(), Local_Data_List.local_tour_data_list);
-            marylee.setLayoutManager(new LinearLayoutManager(getActivity()));
-            marylee.setAdapter(rcvAd2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+            // 테스트 끝
+
+
+
+
+
+
 
         }
 
